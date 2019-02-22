3e456c8cfaccbf3037b869efa66d747cb7eabf54
hwgordon247 <hwgordon247@gmail.com>
2019-02-22 16:58:33 +0000

    bitmex filled order implementation

--- xchange-core/src/main/java/org/knowm/xchange/service/trade/TradeService.java
+++ xchange-core/src/main/java/org/knowm/xchange/service/trade/TradeService.java
@@ -283,4 +283,8 @@ public interface TradeService extends BaseService {
   default String updateOrder(Order order) throws IOException {
     throw new NotYetImplementedForExchangeException();
   }
+
+  default OpenOrders getFilledOrders() throws IOException {
+    throw new NotYetImplementedForExchangeException();
+  }
 }
